752c6a2ab80a9967dfc78fbd57c3285cc2716109
olegboychuk <obagelo23>
2015-10-25 01:46:07 +0200

    starting usage bower_components

--- application/RestControllers/AlbumRestController.php
+++ application/RestControllers/AlbumRestController.php
@@ -2,8 +2,41 @@
 require_once dirname (__FILE__). '/../core/Model/AlbumModel.php';
 require_once dirname (__FILE__). '/../core/Controller/Controller.php';
 ///??? REQUIRE ONCE TO AlbumController
+
 class AlbumRestController extends AlbumController{
 	public $model;
+
+	protected function createAnswer( $error,$answer,$code=200 ){
+		$responce = array(
+			"code"=$code;	
+			);
+
+		if( $error )
+			$responce["error"]=$answer;
+		else
+			$responce["result"]=$answer;
+
+		return $responce;
+	}
+
+	public function searchAlbums( $str ){
+		$album_details = parent::searchAlbums( $str );
+
+		if( is_array( $album_details ) )
+			return $this->createAnswer( 0, array("albums"=>$album_details) );
+		elseif ($album_details == NULL) 
+			return $this->createAnswer( 1,"empty result",402 )
+		else
+		   return $this->buildAnswer( 1, "invalid search", 400 );
+	}
+
+	public function getDetailsAlbum( $id ){
+	     $album_details = parent::searchAlbums( $id );
+
+
+		if( is_array( $album_details ) )
+			return $this->createAnswer( 0, array( "album_details"=>$album_details ) )
+	}
 
 
 }
