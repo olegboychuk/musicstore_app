8cd009ec4c5460d1db1ee9b061a07852daa02b15
olegboychuk <obagelo23>
2015-10-24 20:17:09 +0300

    Server side: REST;CONTROLLERS

---
+++ application/Controllers/AlbumController.php
@@ -0,0 +1,55 @@
+<?php 
+require_once dirname (__FILE__). '/../core/Model/AlbumModel.php';
+require_once dirname (__FILE__). '/../core/Controller/Controller.php';
+
+class AlbumController(){
+	public $model;
+
+	public function __construct(){
+		$this->model = new AlbumModel();
+	}
+
+    /**
+	/*public function searchAlbums
+	/*	FETCH SPECIFIC ALBUMS BY SEARCH FROM DB
+	*/
+	public function searchAlbums( $str ){
+		if ( strlen($str)<3 ) 
+			return false;
+		
+		$album_details = $this->model->searchAlbums( $str );
+		if ( $album_details !== false ) 
+			return $album_details;
+		
+		return false;
+	}
+
+	/**
+	/*public function getALLAlbums
+	/*	FETCH ALL ALBUMS FROM DB
+	*/
+	public function getALLAlbums(){
+		$albums = $this->model->getALLAlbums();
+		
+		if ($albums !== NULL) 
+			return $albums;
+		else
+			return false;	
+	}
+
+
+     /**
+	/*public function getDetailsAlbum
+	/*	FETCH DETAILS OF ALBUM FROM DB
+	*/
+	public function getDetailsAlbum( $id ){
+		$album_details = $this->model->getDetailsAlbum( $id );
+		
+		if( $album_details !== NULL )
+			return $album_details
+		else
+			return false
+	}
+}
+
+?>
--- application/core/Model/AlbumsModel.php
+++ application/core/Model/AlbumsModel.php
@@ -13,11 +13,26 @@ class AlbumModel extends Model{
 	}
 
 	/**
+	/*public function searchAlbums
+	/*	FETCH SPECIFIC ALBUMS BY SEARCH FROM DB
+	*/
+	public function searchAlbums( $str ){
+		$result = $this->_db->query( "SELECT * FROM albums AS a WHERE a.album_name like '%$str%' " );
+		
+		$album_details = array();
+		if ( $result->num_rows>0 ){
+			while ( $row = $result->fetch_assoc() ) 
+				$album_details[]=$row;
+				return = $album_details;		
+	    }
+	}
+
+	/**
 	/*public function getALLAlbums
 	/*	FETCH ALL ALBUMS FROM DB
 	*/
 	public function getALLAlbums(){
-		$result=$this->_db->query("SELECT * FROM albums");
+		$result = $this->_db->query("SELECT * FROM albums");
 
 		if(result){
 			$albums=array();
@@ -28,13 +43,29 @@ class AlbumModel extends Model{
 		}
 	}
 
+     /**
+	/*public function getGroupAlbums
+	/*	FETCH GROUP ALBUMS FOR FIRST GET FROM DB????
+	*/
+	public function getGroupAlbums(){
+		$result=$this->_db->query("SELECT * FROM albums ORDER BY album_id DESC 23 LIMIT");
+
+		if(result){
+			$albums=array();
+			while ($row = $result->fetch_assoc()) 
+			    $albums[]=$row;
+			
+			return $albums;
+		}
+	
+
 	/**
 	/*public function getDetailsAlbum
 	/*	FETCH DETAILS OF ALBUM FROM DB
 	*/
-	public function getDetailsAlbum($id){
+	public function getDetailsAlbum( $id ){
 
-		$result = $this->_db->query(" SELECT * FROM albums WHERE album_id = $id ");
+		$result = $this->_db->query( " SELECT * FROM albums WHERE album_id = $id ");
 			
 		if(result){
 			$album_details=array();
