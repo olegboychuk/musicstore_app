bfd498d0b5cfae46ec7f1ee813ef0703ec32b303
olegboychuk <obagelo23>
2015-11-05 10:23:05 +0200

    home album css(not finished)

--- app/Albums/AlbumController.js
+++ app/Albums/AlbumController.js
@@ -5,22 +5,9 @@ AlbumsModule.controller( 'AlbumController',function( $log,$http,$scope,AlbumsFac
 	console.log( "cAlbumsFactory.getAlbums",AlbumsFactory.getAlbums() );
 	
 	var getAlbums = AlbumsFactory.getAlbums();
-	  getAlbums.then(function(data){
-	  	console.log("albums",data);
+	  getAlbums.then( function( data ){
 	  	$scope.albums = data.albums;
 	  	console.log("$scope.albums",$scope.albums);
 	  });
-     
-	// var promise =  AlbumsFactory.getNext( 2 );
-	// promise.then(
-	//     function( payload ) {
-	//     	console.log("payload",payload);
-	//         $scope.albums = payload;
-	//         console.log("$scope.albums",$scope.albums);
-	//     },
-	//     function(errorPayload) {
-	// 	   $log.error('failure loading albums', errorPayload);
-	// 	}
-	// );
 
 });
