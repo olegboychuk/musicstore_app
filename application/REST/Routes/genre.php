cec0f90b515e23f04f454ff965357ea86e330b90
olegboychuk <oleg.boychuk@gmail.com>
2015-11-10 23:01:03 +0200

    Create genre.php

---
+++ application/REST/Routes/genre.php
@@ -0,0 +1,34 @@
+
+<?php
+
+require_once dirname (__FILE__).'/../../RestControllers/GenreRestController.php';
+$genre = new GenreRestController();
+// var_dump($genre);
+
+// $app->get('/albums/:id',function( $start ) use ( $album, $app ){
+// 	// global $app;
+// 	 $start = explode(':', $start);
+
+// 	$al=$album->getAlbums( $start[1] );
+	
+// 	echo json_encode( $al ); 
+// });
+
+$app->get('/genres/',function() use ( $genre ){
+	$genres=$genre->getAllGenres();
+	
+	echo json_encode( $genres ); 
+});
+
+
+$app->post('',function() use ( $album ){
+
+});
+
+$app->put('',function() use ( $album ){
+
+});
+
+$app->delete('',function() use ( $album ){
+
+});
