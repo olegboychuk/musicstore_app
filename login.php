3b9efe151d0bfaa22d9de620be648031565373d2
kirkaZaz <kirka.zaz@gmail.com>
2015-11-15 19:40:49 +0200

    qqq
    qqq

---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="musicStore">
+<head>
+	<meta charset="UTF-8">
+	<title>Home Page</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="checkout.css">
+</head>
+<body ng-controller="mainController">
+<!-- _____________________________ -->
+<!-- ___________HEADER____________ -->
+<!-- _____________________________ -->
+	<header>
+		<nav>
+			<div class="wrapper">
+				<ul>
+					<a href="index1.html"><li>Home</li></a>
+					<a href="#myAcoount" ng-show=""><li>My Account</li></a>
+					<a href="#myWishlist"><li>My Wishlist</li></a>
+					<a href="check-out-1.html"><li>Checkout</li></a>
+					<a href="#" ng-hide="" ><li>Log In</li></a>
+				</ul>	
+			</div>	
+			</nav>
+		<div id="nav-header">
+			<div class="wrapper">
+				<img class="logo" src="Images/logo.png" >
+				<div id="find-album">
+					<input type="text" class="search" />
+				</div>
+				<div id="store-status"></div>
+			</div>
+		</div>
+	</header>
+	<div class="wrapper">
+		<h1 class="check-out-h1">Login or Create an Account</h1>
+
+		<div id="newCostumers" class="regFile">
+			<header>NEW COSTUMERS</header>
+			<p>By creating an account in our store</p>
+		</div>
+		<div id="registrCostumers" class="regFile">
+			<header>REGISTERED COSTUMERS</header>
+		</div>
+	</div>
+
+	</body>
+	</html>
