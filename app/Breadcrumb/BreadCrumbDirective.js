4a782f60a1035ac6b9bdb707aa6a6dfb3d49aeb2
olegboychuk <obagelo23>
2015-11-27 04:06:51 +0200

    view  resolution changes

--- app/Breadcrumb/BreadCrumbDirective.js
+++ app/Breadcrumb/BreadCrumbDirective.js
@@ -2,7 +2,7 @@ app.directive('breadCrumb',function($route){
 	return {
 		restrict: 'E',
 		templateUrl: 'app/Breadcrumb/_bread-crumb.html',
-        link: function(scope, element, attrs, route) {
+        link: function($scope, element, attrs, $route) {
             console.log('$route.current.name', $route.current.name);
             scope.crumbs = [];
         }
